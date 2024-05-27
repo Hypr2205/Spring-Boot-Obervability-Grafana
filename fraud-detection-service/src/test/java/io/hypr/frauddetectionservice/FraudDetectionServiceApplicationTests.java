edc3522f39f9707426a83560a664b7efb0c0a30c
Hypr <procango2003@gmail.com>
2024-05-27 21:13:49 +0700

    first commit

---
+++ fraud-detection-service/src/test/java/io/hypr/frauddetectionservice/FraudDetectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hypr.frauddetectionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
